e72e4fe0c78a56b61deb0aa36de1c24dd4da7a34
salarissa321 <salarissa321@gmail.com>
2023-05-17 11:48:44 +0200

    Loop-While and Else-python

---
+++ Loop-While and Else-python.py
@@ -0,0 +1,29 @@
+
+
+
+
+#---------------------------------
+#------ Loops => While---------
+#-----------------------------------
+# While Condtion_is_True
+#     Code Will Run
+
+
+
+a = 0
+
+while a <= 10 :
+    print(a) # 0, 1 , 2 , 3 , 4 , 5 , 6 , 7, 8 , 9 , 10
+
+    a += 1 # a = a + 1
+
+else :
+    print("Loop is Done") # Loop is Done # True become False
+
+
+
+# while False :
+#     print("Will not print") # 
+
+
+
